11a8a31c08a92c004f7f89517703a21736284bad
Anika Lindley <anikalindley27@gmail.com>
2021-05-31 22:57:57 -0700

    add coverage

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -4,13 +4,19 @@ test_that("my t.test output is correct", {
   expect_equal(my_t.test(x = test_data, alternative = "two.sided", mu = 0.3)$test_stat, -2.028417, tolerance = 0.01)
 })
 
+test_that("my t.test output is correct", {
+  set.seed(123)
+  test_data <- rnorm(50, 0, 1)
+  expect_equal(my_t.test(x = test_data, alternative = "greater", mu = 0.4)$test_stat, -2.792139, tolerance = 0.01)
+})
+
 test_that("my t.test throws error", {
   set.seed(123)
   test_data <- rnorm(50, 0, 1)
   expect_error(my_t.test(x = test_data, alternative = "error", mu = 0.3))
 })
 
-test_that("my t.test throws error", {
+test_that("my t.test returns list", {
   set.seed(123)
   test_data <- rnorm(50, 0, 1)
   expect_type(my_t.test(x = test_data, alternative = "less", mu = 0.3), "list")
